e1db2c2b58480954dae9d9052ad0fe354d33b85b
Quentin-Jadoul <18155@ecam.be>
2023-05-01 13:27:39 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PlacesListComponent } from './places-list/places-list.component';
+import { ActivitiesListComponent } from './activities-list/activities-list.component';
+
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlacesListComponent,
+    ActivitiesListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'places-list', component: PlacesListComponent},
+      {path: 'activities-list', component: ActivitiesListComponent},
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
